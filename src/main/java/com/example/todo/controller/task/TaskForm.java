0dd563d5c60b38e7423a84a9607d74112ee0370a
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-06-17 23:07:18 +0900

    refactor

--- src/main/java/com/example/todo/controller/task/TaskForm.java
+++ src/main/java/com/example/todo/controller/task/TaskForm.java
@@ -3,14 +3,18 @@ package com.example.todo.controller.task;
 import com.example.todo.service.task.TaskEntity;
 import com.example.todo.service.task.TaskStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record TaskForm (
         @NotBlank
+        @Size(max = 256, message = "256文字以内で入力してください")
         String summary,
 
         String description,
 
         @NotBlank
+        @Pattern(regexp = "TODO|DOING|DONE", message = "Todo, Doing, Doneのいずれかを選択してください")
         String status
 ) {
     public TaskEntity toEntity() {
