7fb3118bfc56deda571c9676829f8eed3a098abc
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-06-19 00:33:32 +0900

    edit_add

---
+++ src/main/java/com/example/todo/controller/task/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.todo.controller.task;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TaskNotFoundException extends RuntimeException {
+}
